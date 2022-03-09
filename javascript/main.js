6d4273848c4865ec72d964c866ca7f3e66cd8b79
Natacha Sica <natachasica@gmail.com>
2022-03-09 20:43:20 -0300

    Primera preentrega

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,40 @@
 const canasta = [];
 const IVA = 1.21;
 
-class Productos{
-    constructor (nombreProducto,cantidad,precioUnidad){
-        this.nombreProducto=nombreProducto,
-        this.cantidad= cantidad
+class Productos {
+    constructor(nombreProducto, cantidad, precioUnidad) {
+        this.nombreProducto = nombreProducto,
+            this.cantidad = cantidad
         this.precioUnidad = precioUnidad;
     }
-   precioFinal (){
+    precioFinal() {
         let resultadoPrecioFinal = cantidad * precioUnidad * IVA;
         return resultadoPrecioFinal;
     }
 }
 
-function calcularCarrito (){
-    nombreProducto = prompt('Ingrese el nombre del producto');
-    cantidad = parseFloat(prompt('Ingrese la cantidad de productos a adquirir'));
-    precioUnidad = parseFloat(prompt('Ingrese el precio de la unidad a adquirir'));
 
-    totalCarrito = new Productos (nombreProducto,cantidad,precioUnidad);
-    canasta.push(totalCarrito);
-    alert('Usted debe abonar:' + totalCarrito.precioFinal());
+function calcularCarrito() {
+    do {
+        nombreProducto = prompt('Ingrese el nombre del producto');
+        cantidad = parseFloat(prompt('Ingrese la cantidad de productos a adquirir'));
+        precioUnidad = parseFloat(prompt('Ingrese el precio de la unidad a adquirir'));
+
+        item = new Productos(nombreProducto, cantidad, precioUnidad);
+        canasta.push(item);
+        nuevaCompra = prompt('Si quiere comprar un nuevo producto ingrese SI o NO');
+    }
+    while (nuevaCompra == "SI" || nuevaCompra == "si");
     console.table(canasta);
 
+    const totalPorProducto = canasta.map(i => i.cantidad * i.precioUnidad * IVA);
+    
+    const totalAPagar = totalPorProducto.reduce((a,b)=> a+b);
+    console.log('Su total a pagar es: $' + totalAPagar);
 }
+
+
+
+
+
 
