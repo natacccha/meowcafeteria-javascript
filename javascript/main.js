a6953f7ae4a6a220c6dec1cd435741817d7781f2
Natacha Sica <natachasica@gmail.com>
2022-03-23 10:50:40 -0300

    PreEntrega 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,154 @@
 const canasta = [];
 const IVA = 1.21;
 
-class Productos {
-    constructor(nombreProducto, cantidad, precioUnidad) {
-        this.nombreProducto = nombreProducto,
-            this.cantidad = cantidad
-        this.precioUnidad = precioUnidad;
-    }
-    precioFinal() {
-        let resultadoPrecioFinal = cantidad * precioUnidad * IVA;
-        return resultadoPrecioFinal;
+
+// Informacion de Productos que se populara Dinamicamente
+
+const informacionDeProductos = [{
+        "titulo": "Peluche Elefante",
+        "Precio": 560,
+        "imagenURL": 'images/juguete1.jpg',
+        "id": "pelucheElefante"
+    },
+    {
+        "titulo": "Peluche Ardillita",
+        "Precio": 520,
+        "imagenURL": 'images/juguete2.jpg',
+        "id": "pelucheArdillita"
+
+    },
+    {
+        "titulo": "Gatito Pelota",
+        "Precio": 1300,
+        "imagenURL": 'images/juguete3.jpg',
+        "id": "gatitoPelota"
+
+    },
+    {
+        "titulo": "Varita Gallina",
+        "Precio": 700,
+        "imagenURL": 'images/juguete4.jpg',
+        "id": "varitaGallina"
+
+    },
+    {
+        "titulo": "Raton Elastico",
+        "Precio": 900,
+        "imagenURL": 'images/juguete5.jpg',
+        "id": "ratonElastico"
+
+    },
+    {
+        "titulo": "Pelota de Goma",
+        "Precio": 1100,
+        "imagenURL": 'images/juguete6.jpg',
+        "id": "pelotaGoma"
+
+    },
+    {
+        "titulo": "Ratoncito a Colores",
+        "Precio": 990,
+        "imagenURL": 'images/juguete7.jpg',
+        "id": "ratoncitoColores"
+
+    },
+    {
+        "titulo": "Patito Cascabel",
+        "Precio": 1200,
+        "imagenURL": 'images/juguete8.jpg',
+        "id": "patitoCascabel"
+
+    },
+    {
+        "titulo": "Ratoncito Catnip",
+        "Precio": 1000,
+        "imagenURL": 'images/juguete9.jpg',
+        "id": "ratoncitoCatnip"
+
+    },
+    {
+        "titulo": "Caballito de Mar",
+        "Precio": 1200,
+        "imagenURL": 'images/juguete10.jpg',
+        "id": "caballitoMar"
+
+    },
+    {
+        "titulo": "Varita para Gatos",
+        "Precio": 1250,
+        "imagenURL": 'images/juguete11.jpg',
+        "id": "varitaGatos"
+
+    },
+    {
+        "titulo": "Cerdito de Goma",
+        "Precio": 1500,
+        "imagenURL": 'images/juguete12.jpg',
+        "id": "cerditoGoma"
+
+    },
+
+]
+
+
+function agregarProductosDinamicos() {
+    const cardProductos = document.getElementById('cardProductos');
+    for (articulos of informacionDeProductos) {
+        const estructura = `<span><div class='box'><img src=${articulos.imagenURL}></div><p class='producto'>${articulos.titulo}</p><p class='precio'>$${articulos.Precio}</p><a class='botonComprar'id=${articulos.id} >Agregar al Carrito</a></span>`
+        cardProductos.innerHTML += estructura;
+
     }
 }
+agregarProductosDinamicos();
 
-function calcularCarrito() {
-    do {
-        nombreProducto = prompt('Ingrese el nombre del producto');
-        cantidad = parseFloat(prompt('Ingrese la cantidad de productos a adquirir'));
-        precioUnidad = parseFloat(prompt('Ingrese el precio de la unidad a adquirir'));
 
-        item = new Productos(nombreProducto, cantidad, precioUnidad);
-        canasta.push(item);
-        nuevaCompra = prompt('Si quiere comprar un nuevo producto ingrese SI o NO');
+//Aqui lo que hacemos es identificar en que producto hizo click el cliente y su carrito.
+
+const botonDeComprar = document.querySelectorAll('.botonComprar');
+botonDeComprar.forEach(boton => {
+    boton.addEventListener('click', () => {
+        var botonId = boton.getAttribute("id");
+        for (productos of informacionDeProductos) {
+            if (productos.id === botonId) {
+                canasta.push(productos);
+            }
+        }
+        agregarFilas();
+        alert('🐶Producto Agregado a tu carrito 😺' );
+    })
+})
+
+
+// Interaccion con el HTML (DOM)
+function agregarFilas() {
+    const tabladeProductos = document.getElementById('tableBody');
+    while (tabladeProductos.hasChildNodes()) {
+        tabladeProductos.removeChild(tabladeProductos.firstChild);
+      }
+    for (datos of canasta) {
+        const td = "<tr><td>" + datos.titulo + "</td><td>" + 1 + "</td><td>" + datos.Precio+ "</td></tr>"
+        tabladeProductos.innerHTML += td;
+        
     }
-    while (nuevaCompra == "SI" || nuevaCompra == "si");
-    console.table(canasta);
+    guardarCarrito();
+} 
 
-    const totalPorProducto = canasta.map(i => i.cantidad * i.precioUnidad * IVA);
-    
-    const totalAPagar = totalPorProducto.reduce((a,b)=> a+b);
-    console.log('Su total a pagar es: $' + totalAPagar);
 
+
+//Storage en memoria
+
+const guardarCarrito = ()=> {
+    localStorage.setItem("CarritoAbandonado",JSON.stringify(canasta));
+}
+
+//Persistencia de datos
+
+const recuperoCarrito = ()=> {
+    var recuperoDeCarrito = localStorage.getItem("CarritoAbandonado");
+    for(recupero of JSON.parse(recuperoDeCarrito)){
+        canasta.push(recupero);
+    }
     agregarFilas();
 }
 
-// Interaccion con el HTML
-
-
-function agregarFilas(){
-    const tabladeProductos = document.getElementById('tableBody');
-    for(datos of canasta){
-        const td = "<tr><td>"+datos.nombreProducto+"</td><td>"+datos.cantidad+"</td><td>"+datos.precioUnidad+"</td></tr>"
-        tabladeProductos.innerHTML+=td;
-    }
-
-    
-
-}
-
-
-
-
-
+recuperoCarrito()
