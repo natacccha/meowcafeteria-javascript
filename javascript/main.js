a1705bc879d1f28a99f3b19c69edf6b1228194d4
Natacha Sica <natachasica@gmail.com>
2022-04-08 16:44:26 -0300

    Desafio AJAX

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,7 @@
 const canasta = [];
 const IVA = 1.21;
 
-// Declaracion de mi archivo JSON
 
-const databaseProductos = `${window.location.origin}/javascript/productos.json`
 
 
 function timedRefresh(timeoutPeriod) {
@@ -13,137 +11,49 @@ function timedRefresh(timeoutPeriod) {
 
 // Informacion de Productos que se populara Dinamicamente
 
-const informacionDeProductos = (databaseProductos) => {
-    fetch(databaseProductos)
+var informacionDeProductos;
+
+    fetch("https://natacccha.github.io/meowcafeteria-javascript/javascript/productos.json")
     .then((response)=> response.json())
-    .then((data) => console.table(data))
-}
-
-/* const informacionDeProductos = [{
-        "titulo": "Peluche Elefante",
-        "Precio": 560,
-        "imagenURL": 'images/juguete1.jpg',
-        "id": "pelucheElefante"
-    },
-    {
-        "titulo": "Peluche Ardillita",
-        "Precio": 520,
-        "imagenURL": 'images/juguete2.jpg',
-        "id": "pelucheArdillita"
-
-    },
-    {
-        "titulo": "Gatito Pelota",
-        "Precio": 1300,
-        "imagenURL": 'images/juguete3.jpg',
-        "id": "gatitoPelota"
-
-    },
-    {
-        "titulo": "Varita Gallina",
-        "Precio": 700,
-        "imagenURL": 'images/juguete4.jpg',
-        "id": "varitaGallina"
-
-    },
-    {
-        "titulo": "Raton Elastico",
-        "Precio": 900,
-        "imagenURL": 'images/juguete5.jpg',
-        "id": "ratonElastico"
-
-    },
-    {
-        "titulo": "Pelota de Goma",
-        "Precio": 1100,
-        "imagenURL": 'images/juguete6.jpg',
-        "id": "pelotaGoma"
-
-    },
-    {
-        "titulo": "Ratoncito a Colores",
-        "Precio": 990,
-        "imagenURL": 'images/juguete7.jpg',
-        "id": "ratoncitoColores"
-
-    },
-    {
-        "titulo": "Patito Cascabel",
-        "Precio": 1200,
-        "imagenURL": 'images/juguete8.jpg',
-        "id": "patitoCascabel"
-
-    },
-    {
-        "titulo": "Ratoncito Catnip",
-        "Precio": 1000,
-        "imagenURL": 'images/juguete9.jpg',
-        "id": "ratoncitoCatnip"
-
-    },
-    {
-        "titulo": "Caballito de Mar",
-        "Precio": 1200,
-        "imagenURL": 'images/juguete10.jpg',
-        "id": "caballitoMar"
-
-    },
-    {
-        "titulo": "Varita para Gatos",
-        "Precio": 1250,
-        "imagenURL": 'images/juguete11.jpg',
-        "id": "varitaGatos"
-
-    },
-    {
-        "titulo": "Cerdito de Goma",
-        "Precio": 1500,
-        "imagenURL": 'images/juguete12.jpg',
-        "id": "cerditoGoma"
-
-    },
-
-] */
-
+    .then(data => {
+        informacionDeProductos = data; 
+        agregarProductosDinamicos();
+      });
 
 function agregarProductosDinamicos() {
     const cardProductos = document.getElementById('cardProductos');
-    for (articulos of informacionDeProductos) {
-        const estructura = `<span><div class='box'><img src=${articulos.imagenURL}></div><p class='producto'>${articulos.titulo}</p><p class='precio'>$${articulos.Precio}</p><a class='botonComprar'id=${articulos.id} >Agregar al Carrito</a></span>`
+     for (articulos of informacionDeProductos) {
+        const estructura = `<span><div class='box'><img src=${articulos.imagenURL}></div><p class='producto'>${articulos.titulo}</p><p class='precio'>$${articulos.Precio}</p><a href="javascript:botonComprar(${articulos.id})" class='botonComprar'id=${articulos.id} >Agregar al Carrito</a></span>`
         cardProductos.innerHTML += estructura;
-
-    }
+    } 
 }
-agregarProductosDinamicos();
 
 
 //Aqui lo que hacemos es identificar en que producto hizo click el cliente y su carrito.
 
-const botonDeComprar = document.querySelectorAll('.botonComprar');
-botonDeComprar.forEach(boton => {
-    boton.addEventListener('click', () => {
-        var botonId = boton.getAttribute("id");
-        for (productos of informacionDeProductos) {
-            if (productos.id === botonId) {
-                canasta.push(productos);
-            }
+
+function botonComprar(boton){
+    for (productos of informacionDeProductos) {
+        if (productos.id === boton.id) {
+            canasta.push(productos);
         }
-        agregarFilas();
-        Toastify({
-            text: "✔ Agregado al Carrito 🐶😺 ",
-            duration: 3000,
-            newWindow: true,
-            close: true,
-            gravity: "top", 
-            position: "right", 
-            stopOnFocus: true, 
-            style: {
-                background: "grey",
-            },
-            onClick: function () {} // Callback after click
-        }).showToast();
-    })
-})
+    }
+    agregarFilas();
+    Toastify({
+        text: "✔ Agregado al Carrito 🐶😺 ",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+            background: "grey",
+        },
+        onClick: function () {} 
+    }).showToast();
+}
+
 
 
 // Interaccion con el HTML (DOM)
@@ -154,7 +64,7 @@ function agregarFilas() {
     }
     for (var i = 0; i < canasta.length; i++) {
         var datos = canasta[i];
-        const td = "<tr><td>"+i+"</td><td>" + datos.titulo + "</td><td>" + 1 + "</td><td>" + datos.Precio + "</td><td><button id='"+i+"' class='borradoIndividual'>❌</button></td></tr>" 
+        const td = `<tr><td>${i}</td><td>${datos.titulo}</td><td> 1  </td><td>  ${datos.Precio} </td><td><button onclick='borradoIndividualProdu(${i})' id=${i} class='borradoIndividual'>❌</button></td></tr> `
         tabladeProductos.innerHTML += td;
      }
 
@@ -216,15 +126,9 @@ eliminarProductos.addEventListener('click', () => {
 
 //Borrado individual de productos
 
-const borradoIndividual = document.querySelectorAll('.borradoIndividual');
-borradoIndividual.forEach(boton => {
-    boton.addEventListener('click', () => {
-        var botonId = boton.getAttribute("id");
-        canasta.splice(parseInt(botonId),1);
-       location.reload()
-        guardarCarrito();
-        recuperoCarrito();
-    })
-})
 
-
+function borradoIndividualProdu(boton){
+    canasta.splice(parseInt(boton),1);
+    guardarCarrito();
+    recuperoCarrito();
+}
